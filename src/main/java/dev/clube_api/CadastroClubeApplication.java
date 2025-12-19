fff8e1f491ac1f22845a8ec1bce22b72755e0b5e
Luis-Felipe-Santos <luysfelyp07@gmail.com>
2025-12-19 16:14:11 -0300

    first commit

---
+++ src/main/java/dev/clube_api/CadastroClubeApplication.java
@@ -0,0 +1,13 @@
+package dev.clube_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroClubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroClubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/clube_api/CadastroClubeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.clube_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroClubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
