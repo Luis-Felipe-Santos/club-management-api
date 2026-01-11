2c7ef576998294313940b59c848192a374de1639
Luis-Felipe-Santos <luysfelyp07@gmail.com>
2026-01-11 20:50:07 -0300

    feat: adicionar swagger para documentação

--- src/main/java/dev/clube_api/config/SecurityConfig.java
+++ src/main/java/dev/clube_api/config/SecurityConfig.java
@@ -41,7 +41,9 @@ public class SecurityConfig {
                         .authenticationEntryPoint(authenticationEntryPoint)
                 )
 
+
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
