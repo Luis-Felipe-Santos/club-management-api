32a50b6284de0b90e43704b7b40ecaaf17ac7f17
Luis-Felipe-Santos <luysfelyp07@gmail.com>
2026-03-16 17:06:22 -0300

    feat: adicionar regras de Cors

--- src/main/java/dev/clube_api/config/SecurityConfig.java
+++ src/main/java/dev/clube_api/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
